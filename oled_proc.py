bbc7e239b26c4cd715b99602fc9602d93873957c
Andrew Villeneuve <andrewmv@gmail.com>
2019-08-09 23:15:49 +0000

    Bandwidth minigraphs, refacoring

--- oled_proc.py
+++ oled_proc.py
@@ -24,6 +24,7 @@ device = ssd1306(serial, rotate=2)
 hist_size = 32                 # Horizontal width (in px) of minigraphs
 iface = 'enp1s0'               # Network interface to monitor
 refresh = 1                    # Seconds per refresh
+wire_speed = (1024 * 1024 * 125) # For scaling bandwidth minigraph
 
 ## Init Data ##
 frame = 0       # Refresh counter
@@ -54,40 +55,67 @@ def getloadavg():
     load = os.getloadavg()
     return '{one:.2}, {five:.2}, {fifteen:.2}'.format(one=load[0], five=load[1], fifteen=load[2])
 
+def txgraph(draw, x, y, height, bps):
+    global tx_hist_i
+    bar_height = (int)((bps / wire_speed) * height)
+    tx_hist[tx_hist_i] = bar_height
+    draw_graph(draw, x, y, height, tx_hist, tx_hist_i, bar_height)
+    tx_hist_i += 1
+    if tx_hist_i >= hist_size:
+        tx_hist_i = 0
+
+def rxgraph(draw, x, y, height, bps):
+    global rx_hist_i
+    bar_height = (int)((bps / wire_speed) * height)
+    rx_hist[rx_hist_i] = bar_height
+    draw_graph(draw, x, y, height, rx_hist, rx_hist_i, bar_height)
+    rx_hist_i += 1
+    if rx_hist_i >= hist_size:
+        rx_hist_i = 0
+
 def cpugraph(draw, x, y, height):
     global cpu_hist_i
     cpu = psutil.cpu_percent()
     bar_height = (int)((cpu / 100) * height)
     cpu_hist[cpu_hist_i] = bar_height 
-    for i in range(cpu_hist_i + 1, hist_size):
-        draw.line([ x + i - cpu_hist_i, \
-                    y + height, \
-                    x + i - cpu_hist_i, \
-                    y + height - cpu_hist[i]], \
-                   fill="white", \
-                   width=1)
-    for i in range(0, cpu_hist_i):
-        draw.line([ x + hist_size - cpu_hist_i + i, \
-                    y + height, \
-                    x + hist_size - cpu_hist_i + i, \
-                    y + height - cpu_hist[i]], \
-                   fill="white", \
-                   width=1)
+    draw_graph(draw, x, y, height, cpu_hist, cpu_hist_i, bar_height)
     cpu_hist_i += 1
     if cpu_hist_i >= hist_size:
         cpu_hist_i = 0
 
+def draw_graph(draw, x, y, height, stat_hist, stat_hist_i, bar_height):
+    for i in range(stat_hist_i + 1, hist_size):
+        draw.line([ x + i - stat_hist_i, \
+                    y + height, \
+                    x + i - stat_hist_i, \
+                    y + height - stat_hist[i]], \
+                   fill="white", \
+                   width=1)
+    for i in range(0, stat_hist_i):
+        draw.line([ x + hist_size - stat_hist_i + i, \
+                    y + height, \
+                    x + hist_size - stat_hist_i + i, \
+                    y + height - stat_hist[i]], \
+                   fill="white", \
+                   width=1)
+
 def getrxspeed():
     global rx_sample
     new_sample = psutil.net_io_counters().bytes_recv
-    bps = new_sample - rx_sample
+    if rx_sample == 0:
+        bps = 0
+    else:
+        bps = new_sample - rx_sample
     rx_sample = new_sample
     return bps
 
 def gettxspeed():
     global tx_sample
     new_sample = psutil.net_io_counters().bytes_sent
-    bps = new_sample - tx_sample
+    if tx_sample == 0:
+        bps = 0
+    else:
+        bps = new_sample - tx_sample
     tx_sample = new_sample
     return bps
 
@@ -127,7 +155,8 @@ while True:
 
         str = getloadavg()
         draw.text((x, y), str, fill="white")
-        y += draw.textsize(str)[1] + 1
+        #y += draw.textsize(str)[1] + 1
+        y += 10
         cpugraph(draw, device.width - hist_size - 3, y - 10, 10)
 
         # Line 3: Rx - minigraph
@@ -135,10 +164,18 @@ while True:
         rx = getrxspeed()
         str = 'Rx: ' + pretty_speed(rx)
         draw.text((x, y), str, fill="white")
-        y += draw.textsize(str)[1] + 1
-        #rxgraph(draw, device.width - hist_size - 3, y - 10, 10)
+        y += 10
+        rxgraph(draw, device.width - hist_size - 3, y - 10, 10, rx)
 
-    time.sleep(1)
+        # Line 4: Tx - minigraph
+
+        tx = gettxspeed()
+        str = 'Tx: ' + pretty_speed(tx)
+        draw.text((x, y), str, fill="white")
+        y += 10
+        txgraph(draw, device.width - hist_size - 3, y - 10, 10, tx) 
+
+    time.sleep(refresh)
     frame+=1
     if (frame > 256):
         frame = 0
